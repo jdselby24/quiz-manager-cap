3d52c61cbf2015280fd8e3bd8009958243e168a8
jdselby-cap <joshua.selby@capgemini.com>
2022-05-25 09:28:19 +0100

    Add fetchQuiz function to QuizViewer

--- client/src/QuizViewer/QuizViewer.js
+++ client/src/QuizViewer/QuizViewer.js
@@ -1,22 +1,38 @@
 function QuizViewer(props) {
 
-    // This const is filling in for data that will actually be fetched from the API
-    const questions = [
-        {
-            number: 1,
-            title: 'In which country is Paris?'
-        },
-        {
-            number: 2,
-            title: 'What is the largest mammal?'
-        }
-    ];
+    // This function is responsible for fetching quiz data from the API server
+    // NOTE: in this prototype it is simply accessing a const which is storing the data
+    function fetchQuiz(quizName) {
+        const quizes = {
+            "Geography": [
+                {
+                    number: 1,
+                    title: 'In which country is Paris?'
+                },
+                {
+                    number: 2,
+                    title: 'What is the largest continent?'
+                }
+            ],
+            "Computer Science": [
+                {
+                    number: 1,
+                    title: 'What is binary?'
+                },
+                {
+                    number: 2,
+                    title: 'What is the function of the CPU?'
+                }
+            ]
+        };
 
+        return quizes[quizName];
+    }
     // This const is used to hold the HTML generated to display the questions for the relevant quiz
     const questionsHtml = []
 
     // This foreach generates HTML for each question provided by the API for the relevant quiz
-    questions.forEach((question) => {
+    fetchQuiz("Geography").forEach((question) => {
         questionsHtml.push(
             <tr>
                 <td>{question.number}</td>
